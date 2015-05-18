65ef6ea5753ecaabee48aa1404118f04bd5442c2
jtezanos <jason.tezanos@gmail.com>
2015-05-18 09:22:40 -0700

    Initial Commit

---
+++ Horror Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Horror Watch
+//
+//  Created by Jason Tezanos on 5/18/15.
+//  Copyright (c) 2015 We Make Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
